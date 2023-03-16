029918a30fc1c89f5e37eef82612ca62864e0e27
ElizaMaca <124578737+ElizaMaca@users.noreply.github.com>
2023-03-16 22:18:16 +0000

    second

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import heapq
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
+    #  write the function for simulating parallel tasks
     mytrea=[(0,j)for j in range(n)]
     heapq.heapify(mytrea)
     # create the output pairs
-    for s, r in enumerate(data):
+    for j, r in enumerate(data):
         termlaiks, source = heapq.heappop(mytrea)
         output.append(source, termlaiks)
         heapq.heappush(mytrea,(termlaiks+r, source))
     return output
 
 def main():
-    # TODO: create input from keyboard
+    # create input from keyboard
     # input consists of two lines
     #m=map(int,input().split())
     # first line - n and m
@@ -25,9 +25,9 @@ def main():
     # second line - data 
     data=list(map(int,input().split()))
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    # TODO: create the function
+    # create the function
     result = parallel_processing(n,m,data)
-    # TODO: print out the results, each pair in it's own line
+    # print out the results, each pair in it's own line
     for source, seconds in result:
         print(source,seconds)
 if __name__ == "__main__":
